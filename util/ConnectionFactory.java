9dc0bc5a403c1d4ef45e3578ffdf6ce618ec775a
Dayane <dayanetm@outlook.com>
2023-03-16 12:35:29 -0300

    projeto-todoapp

--- util/ConnectionFactory.java
+++ util/ConnectionFactory.java
@@ -2,6 +2,8 @@ package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class ConnectionFactory {
 
@@ -45,6 +47,40 @@ public class ConnectionFactory {
             if (connection != null) {
                 connection.close();
             }
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao fechar a conexão com o banco de dados", ex);
+        }
+    }
+
+    public static void closeConnection(Connection connection, PreparedStatement statement) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+
+            if(statement != null) {
+                statement.close();
+            }
+
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao fechar a conexão com o banco de dados", ex);
+        }
+    }
+
+    public static void closeConnection(Connection connection, PreparedStatement statement, ResultSet resultSet) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+
+            if(statement != null) {
+                statement.close();
+            }
+
+            if(resultSet != null) {
+                resultSet.close();
+            }
+
         } catch (Exception ex) {
             throw new RuntimeException("Erro ao fechar a conexão com o banco de dados", ex);
         }
